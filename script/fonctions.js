a745966545f5c63fc450a8e75ab448cf6e7eaac3
Gableb <gableb@hotmail.fr>
2017-10-14 00:02:49 +0200

    Create fonctions.js

---
+++ script/fonctions.js
@@ -0,0 +1,22 @@
+var filePath='text.txt'
+
+$(document).ready(function() {
+	// timer = setInterval(function(){readTextFile(filePath)}, 500);
+	readTextFile(filePath);
+});
+
+function readTextFile(filePath){
+			$.get(filePath, function(data) {
+				console.log(data);
+				var lines = data.split('\r\n');
+				var tab2;
+				for (var i = 0; i < lines.length; i++) {
+					tab2 = lines[i].split('/');
+					for (var j = 0; j < tab2.length; j++) {
+						$('#yo').append(tab2[j]+'\t');
+					}
+					$('#yo').append('<br>');
+				}
+				//$('#yo').text(data);
+			}, 'text');
+}
