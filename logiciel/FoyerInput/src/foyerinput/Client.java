ca7ec14bdba5afef92a9737be919d875107d39b0
BlivionIaG <kev29lt@gmail.com>
2017-10-16 07:43:00 +0200

    nouveau fichier : logiciel/FoyerInput/clients.txt 	modifié :         logiciel/FoyerInput/src/foyerinput/Client.java 	modifié :         logiciel/FoyerInput/src/foyerinput/FoyerInput.java

--- logiciel/FoyerInput/src/foyerinput/Client.java
+++ logiciel/FoyerInput/src/foyerinput/Client.java
@@ -13,33 +13,57 @@ import controlP5.Button;
  *
  * @author kletor21
  */
-public class Client {
+public class Client extends PApplet {
 
     public Client(String clientName, PApplet window) {
         this.name = clientName;
         this.info = false;
-        this.display = new ControlP5(window);
+        destroy = false;
+        display = new ControlP5(window);
+
+        remove = display.addButton("remove").setValue(0).plugTo(this, "remove").hide();
+        toggleInfo = display.addButton("toggleInfo").setValue(0).plugTo(this, "toggleInfo").hide();
     }
 
-    public void draw(PApplet window, int x, int y, int size) {
-        window.textSize(size);
-        window.text(name, x, y);
+    public void draw(PApplet window, int x, int y, int length, int wsize, int hsize) {
+        if (!destroy) {
+            window.textSize(hsize);
+            window.text(name, x, y + hsize);
+
+            toggleInfo.setPosition(length + x, y).setSize(wsize, hsize).show();
+            if (info) {
+                remove.setPosition(length + x + wsize, y).setSize(wsize,hsize).show();
+            }
+        }
     }
-    
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    
-    public void setInfo(boolean newState){
+
+    public void setInfo(boolean newState) {
         this.info = newState;
     }
-    
-    public boolean getInfo(){
+
+    public boolean getInfo() {
         return info;
     }
 
+    public void toggleInfo() {
+        info = !info;
+    }
+
+    public void remove() {
+        display.hide();
+        destroy = true;
+    }
+    
+    public boolean getDestroy(){
+        return destroy;
+    }
+
     private String name;
-    private boolean info;
+    private boolean info, destroy;
     private ControlP5 display;
-    private Button toogleInfo, remove;
+    private Button toggleInfo, remove;
 }
--- logiciel/FoyerInput/src/foyerinput/FoyerInput.java
+++ logiciel/FoyerInput/src/foyerinput/FoyerInput.java
@@ -27,8 +27,17 @@ import controlP5.Textfield;
 public class FoyerInput extends PApplet {
 
     void loadConfig(String path) {
+        x_factor = (float) 1 / 32;
+        y_factor = (float) 1 / 32;
+        w_factor = (float) 3 / 4;
+        h_factor = (float) 1 / 8;
+
+        screen_width = 800;
+        screen_height = 600;
+
         fullscreen = false;
         clientsPath = "clients.txt";
+        permanentPath = "permanent.txt";
 
         List<String> cfg = new ArrayList<>();
 
@@ -45,6 +54,18 @@ public class FoyerInput extends PApplet {
                 fullscreen = tmp[1].toUpperCase().equals("TRUE");
             } else if (tmp[0].equals("CLIENTS_PATH")) {
                 clientsPath = tmp[1];
+            } else if (tmp[0].equals("X_FACTOR")) {
+                x_factor = Float.parseFloat(tmp[1]);
+            } else if (tmp[0].equals("Y_FACTOR")) {
+                y_factor = Float.parseFloat(tmp[1]);
+            } else if (tmp[0].equals("W_FACTOR")) {
+                w_factor = Float.parseFloat(tmp[1]);
+            } else if (tmp[0].equals("H_FACTOR")) {
+                h_factor = Float.parseFloat(tmp[1]);
+            } else if (tmp[0].equals("SCREEN_WIDTH")) {
+                screen_width = Integer.parseInt(tmp[1]);
+            } else if (tmp[0].equals("SCREEN_HEIGHT")) {
+                screen_height = Integer.parseInt(tmp[1]);
             }
         }
     }
@@ -53,7 +74,7 @@ public class FoyerInput extends PApplet {
         clients = new ArrayList<>();
 
         loadConfig("config.txt");
-        size(800, 600);
+        size(screen_width, screen_height);
         if (fullscreen) {
             fullScreen();
         }
@@ -61,30 +82,56 @@ public class FoyerInput extends PApplet {
 
     public void setup() {
         surface.setResizable(true);
+        frameRate(30);
+
         gui = new ControlP5(this);
 
         gui.addTextfield("clientName")
-                .setPosition(width / 32, 0)
-                .setSize(width * 3 / 4, height / 8)
+                .setPosition(Math.round(width * x_factor), Math.round(height * y_factor))
                 .setFont(createFont("arial", 20))
                 .setAutoClear(false)
                 .getCaptionLabel().hide();
 
         gui.addButton("addClient")
                 .setValue(0)
-                .setPosition(width * 3 / 4 + width * 3 / 32, 0)
-                .setSize(width / 8, height / 8)
+                .setPosition(width * (2 * x_factor + w_factor), height * y_factor)
                 .getCaptionLabel().hide();
     }
 
     public void draw() {
-        gui.get(Textfield.class, "clientName").setSize(width * 3 / 4, height / 8);
-        gui.get(Button.class, "addClient").setSize(width / 8, height / 8);
+        gui.get(Textfield.class, "clientName").setSize(Math.round(width * w_factor), Math.round(height * h_factor));
+        gui.get(Button.class, "addClient").setSize(Math.round(width * (x_factor * 5)), Math.round(height * h_factor));
         background(192);
 
         int limit = clients.size();
         for (int i = 0; i < limit; i++) {
-            clients.get(i).draw(this, 0, i * height / 8, height / 8);
+            clients.get(i).draw(
+                    this,
+                    Math.round(width * x_factor),
+                    Math.round(height * ((i + 1) * h_factor + y_factor)),
+                    Math.round(width * (x_factor + w_factor)),
+                    Math.round(width * (x_factor * 5 / 2)),
+                    Math.round(height * h_factor)
+            );
+        }
+
+        update();
+    }
+
+    public void update() {
+        updateList(clients);
+        sortClients();
+        updateFile(clientsPath, clients);
+    }
+
+    public void updateList(List<Client> list) {
+        for (int i = 0; i < list.size(); i++) {
+            while (list.get(i).getDestroy()) {
+                list.remove(i);
+                if (i == 0) {
+                    return;
+                }
+            }
         }
     }
 
@@ -98,6 +145,22 @@ public class FoyerInput extends PApplet {
         }
     }
 
+    public void sortClients() {
+        List<Client> tmp = new ArrayList<>();
+        for (Client it : clients) {
+            if (it.getInfo()) {
+                tmp.add(it);
+            }
+        }
+        for (Client it : clients) {
+            if (!it.getInfo()) {
+                tmp.add(it);
+            }
+        }
+
+        clients = tmp;
+    }
+
     public void addClient() {
         Textfield tmp = gui.get(Textfield.class, "clientName");
 
@@ -106,7 +169,7 @@ public class FoyerInput extends PApplet {
         }
 
         clients.add(new Client(tmp.getText(), this));
-        updateFile(clientsPath, clients);
+        update();
         tmp.clear();
     }
 
@@ -114,9 +177,12 @@ public class FoyerInput extends PApplet {
         PApplet.main("foyerinput.FoyerInput");
     }
 
+    private float x_factor, y_factor, w_factor, h_factor;
+    private int screen_width, screen_height;
+
     private ControlP5 gui;
     private List<Client> clients;
 
     private boolean fullscreen;
-    private String clientsPath;
+    private String clientsPath, permanentPath;
 }
